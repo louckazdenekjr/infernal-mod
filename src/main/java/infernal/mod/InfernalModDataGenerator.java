1791285840d5fcd425ce4717efafe4d4eb668c7d
louckazdenekjr <louckazdenekjr@gmail.com>
2023-08-24 15:37:17 +0200

    moved tags to datagen

--- src/main/java/infernal/mod/InfernalModDataGenerator.java
+++ src/main/java/infernal/mod/InfernalModDataGenerator.java
@@ -1,10 +1,13 @@
 package infernal.mod;
 
+
+import infernal.mod.data.ModBlockTagProvider;
 import infernal.mod.data.ModLootTableGenerator;
 import infernal.mod.data.ModModelProvider;
 import infernal.mod.data.ModRecipeGenerator;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
 
 public class InfernalModDataGenerator implements DataGeneratorEntrypoint {
     @Override
@@ -14,5 +17,6 @@ public class InfernalModDataGenerator implements DataGeneratorEntrypoint {
         pack.addProvider(ModLootTableGenerator::new);
         pack.addProvider(ModRecipeGenerator::new);
         pack.addProvider(ModModelProvider::new);
+        pack.addProvider(ModBlockTagProvider::new);
     }
 }
