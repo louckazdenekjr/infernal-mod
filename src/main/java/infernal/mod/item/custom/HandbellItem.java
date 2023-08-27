982be1875608b23cffa65051cc90ecf7e2ac72d3
louckazdenekjr <louckazdenekjr@gmail.com>
2023-08-27 21:23:51 +0200

    prototyping handbell function

--- src/main/java/infernal/mod/item/custom/HandbellItem.java
+++ src/main/java/infernal/mod/item/custom/HandbellItem.java
@@ -1,14 +1,13 @@
 package infernal.mod.item.custom;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.passive.CatEntity;
+import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.player.ItemCooldownManager;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUsageContext;
-import net.minecraft.predicate.entity.EntityPredicate;
 import net.minecraft.predicate.entity.EntityPredicates;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
@@ -18,7 +17,6 @@ import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 
@@ -33,37 +31,42 @@ public class HandbellItem extends Item {
         ItemCooldownManager cooldownManager = user.getItemCooldownManager();
         boolean isCoolingDown = cooldownManager.isCoolingDown(this);
 
-        // play sound effect
-        user.playSound(SoundEvents.BLOCK_BELL_USE, 1.0f, 1.4f);
+        if (!isCoolingDown) { // if ready
+            // both logical sides
+            // start cooldown
+            cooldownManager.set(this, 60);
 
-        if (world.isClient) {
-            // client side
-        } else {
-            // server side
-            if (!isCoolingDown) { // if ready
-                ArrayList entities = (ArrayList) world.getEntitiesByType(
-                        TypeFilter.instanceOf(net.minecraft.entity.passive.CatEntity.class),
-                        new Box(
-                                user.getX() - 32,
-                                user.getY() - 32,
-                                user.getZ() - 32,
-                                user.getX() + 32,
-                                user.getY() + 32,
-                                user.getZ() + 32
-                        ),
-                        EntityPredicates.VALID_ENTITY
-                );
+            if (world.isClient) {
+                // client side
+                // play sound effect
+                user.playSound(SoundEvents.BLOCK_BELL_USE, 1.0f, 1.4f);
+            } else {
+                // server side
+                if (!isCoolingDown) { // if ready
+                    ArrayList entities = (ArrayList) world.getEntitiesByType(
+                            TypeFilter.instanceOf(CatEntity.class),
+                            new Box(
+                                    user.getX() - 32,
+                                    user.getY() - 32,
+                                    user.getZ() - 32,
+                                    user.getX() + 32,
+                                    user.getY() + 32,
+                                    user.getZ() + 32
+                            ),
+                            EntityPredicates.VALID_ENTITY
+                    );
 
-                // report entity count
-                user.sendMessage(Text.literal("Found " + entities.size() + " cats!"), true);
+                    // report entity count
+                    user.sendMessage(Text.literal("Found " + entities.size() + " cats!"), true);
 
-                // start cooldown
-                cooldownManager.set(this, 60);
-
-                // this needs to be last, cuz java no likey
-                for (int i = 0; i <= entities.size(); i++) {
-                    Entity target = (Entity) entities.get(i);
-                    target.addVelocity(0, 4, 0);
+                    // this needs to be last, cuz java no likey
+                    for (int i = 0; i <= entities.size(); i++) {
+                        Entity target = (Entity) entities.get(i);
+                        if (target instanceof TameableEntity) {
+                                TameableEntity target_tameable =  (TameableEntity) target;
+                                target_tameable.setInSittingPose(true);
+                        }
+                    }
                 }
             }
 
